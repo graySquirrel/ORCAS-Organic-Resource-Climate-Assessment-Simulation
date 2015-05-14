901b6e9dbf619ab3d2b35116a37bf29a34082169
graySquirrel <fritzebner@gmail.com>
2015-05-14 12:14:02 -0400

    updated

--- simuTreat.R
+++ simuTreat.R
@@ -1,27 +1,32 @@
-# simuTreat.R calls treatmentClass.R objects to simulate treatments
+# simuTreat.R to simulate treatments
 #    of different feedstocks to generate GHG and cost outputs.
 
 setwd("C:/Users/febner/Documents/CourseraDataScience/fracGASM")
-# source the file because i not yet made package (am lazy)
-if(!exists('treatmentClasses_R')){
-    source("treatmentClasses.R")
-    treatmentClasses_R<-T
-}
-source("treatmentClasses.R") #remove after debug
-
-# go to town to create feedstocks and then treatment them.
-
-f1 <- Feedstock(type=c("GTW","Whey","Apple Pomace","Bakery Waste"),
-                TS=c(17,7,22,92),
-                TVS=c(18,5,0,89),
-                Bo=c(887,300,0,465),
-                TKN=c(5600,580,1100,2100))
-
-
+source("treatmentClasses.R") 
+source("treatmentAnaerobicDigestion.R") 
+#TS=c(17,7,22,92)
+#TVS=c(18,5,0,89)
+#type=c("GTW","Whey","Apple Pomace","Bakery Waste")
+#LoVec <- c(161,15,0,413)
+#TKNVec <- c(5600,580,1100,2100)
+# repeat TKNVec 10 times in a row
+size <- 10
+TKNVec <- seq(from =580, to=5600,length.out=size)
+LoVec <-  seq(from = 15, to=413,length.out=size)
+TKNVecRep <- rep(TKNVec,each=size)
+LoVecRep  <- rep(LoVec,times=size)
+f1 <- Feedstock(type="GTW",
+                TS=17,
+                TVS=18,
+                Lo=LoVecRep,
+                TKN=TKNVecRep)
 #print(paste("length is ",length(f1)))
 #print(paste("type is ", typeof(f1)))
-
 g1 <- GlobalFactors()
-
-res <- AnaerobicDigestionTreatmentPathway(f1, g1, 20)
-print (res)
+res <- AnaerobicDigestionTreatmentPathway(f1, g1, 20,debug = F)
+dfres <- data.frame(LoVecRep,TKNVecRep,res)
+tabres <- xtabs(res ~ LoVecRep+TKNVecRep, data=dfres)
+persp(LoVec,TKNVec,tabres,theta=30,phi=30,shade=0.25,col="lightblue",ticktype="detailed")
+#opens an interactive session
+#persp3d(LoVec,TKNVec,tabres,theta=30,phi=30,ticktype="detailed")
+#print (dfres)
--- treatmentClasses.R
+++ treatmentClasses.R
@@ -6,14 +6,9 @@
 #      methods: GlobalFactors()
 #                   returns GlobalFactors object
 #   Feedstock:
-#      members: type,TS,TVS,Bo,TKN)
-#      methods: Feedstock(type="string",TS=0,TVS=0,Bo=0,TKN=0)
+#      members: type,TS,TVS,Lo,TKN)
+#      methods: Feedstock(type="string",TS=0,TVS=0,Lo=0,TKN=0)
 #                   returns Feedstock object
-#
-# Functions
-#   AnaerobicDigestionTreatmentPathway(Feedstock, GlobalFactors, XportToField = 20)
-#       returns data.frame of factor vs. outputs, labeled with row and col names
-#
 #
 ########### GlobalFactors stuff
 GlobalFactors <- function()
@@ -47,19 +42,18 @@ GlobalFactors <- function()
 }
 
 ########### FeedStock stuff
-Feedstock <- function(type="dontKnow",TS=0,TVS=0,Bo=0,TKN=0)
+Feedstock <- function(type="dontKnow",TS=0,TVS=0,Lo=0,TKN=0)
 {
     if (!is.numeric(TS) || !is.numeric(TVS) || 
-            !is.numeric(Bo) || !is.numeric(TKN) ||
+            !is.numeric(Lo) || !is.numeric(TKN) ||
             !all(is.finite(TS)) || !all(is.finite(TVS)) ||
-            !all(is.finite(Bo)) || !all(is.finite(TKN)))
+            !all(is.finite(Lo)) || !all(is.finite(TKN)))
         stop("invalid input")
-    if (length(TS) != length(TVS) || (length(TS) != length(Bo)) ||
-            (length(TS) != length(TKN)) || (length(TS) != length(type)))
+    if (length(Lo) != length(TKN))
         stop("lengths differ")
     me <- list(
         type = type,
-        TS=TS,TVS=TVS,Bo=Bo,Lo=Bo*TVS/100,TKN=TKN
+        TS=TS,TVS=TVS,Lo=Lo,Bo=Lo*100/TVS,TKN=TKN
     )
     
     ## Set the name for the class
@@ -71,84 +65,6 @@ length.Feedstock <- function(obj) length(obj$TS)
 # create a generic to use in the Feedstock class
 typeof <- function(obj) UseMethod("typeof")
 typeof.Feedstock <- function(obj) obj$type
-
-################# Treatment Functions
-debug<-F
-AnaerobicDigestionTreatmentPathway <- function(Feedstock, GlobalFactors, XportToField = 20)
-{
-    # Step 1: calculate Digester emissions kgCO2e/MT
-    meth       <- Feedstock$Lo * GlobalFactors$AD_Digester_utilizationFactor
-    if(debug) print(paste("meth ",meth))
-    leakTmp    <- meth * GlobalFactors$AD_Digester_CH4Leaks
-    CH4Leaks   <- leakTmp * GlobalFactors$density_CH4 * GlobalFactors$GWPCH4
-    incompTmp  <- meth * GlobalFactors$AD_Digester_incompleteCombustion
-    incomplete <- incompTmp * GlobalFactors$density_CH4 * GlobalFactors$GWPCH4
-    elecTmpTmp <- GlobalFactors$AD_Digester_conversionEfficiency * meth/1000
-    if(debug) print(paste("elecTmpTmp ",elecTmpTmp))
-    elecTmp    <- elecTmpTmp * (1 - 0.12)
-    if(debug) print(paste("elecTmp ",elecTmp))
-    avoidedElecEmissions <- elecTmp * GlobalFactors$AD_Digester_avoidedElectricityEmissions
-    if(debug) print(paste("CH4Leaks ",CH4Leaks," incomplete ",incomplete,
-                          " avoided ",avoidedElecEmissions))
-    Digester   <- CH4Leaks + incomplete + avoidedElecEmissions
-    if(debug) print(paste("Digester ",Digester))
-    
-    # Step2: calculate Storage emissions kgCO2e/MT
-    TVSinDigestate    <- Feedstock$TVS * GlobalFactors$AD_Storage_reductionInVS
-    if(debug) print(paste("TVSinDigestate ",TVSinDigestate))    
-    residualMethane   <- TVSinDigestate * GlobalFactors$AD_Storage_residualMethane
-    if(debug) print(paste("residualMethane ",residualMethane))    
-    effluentEmissions <- residualMethane * GlobalFactors$density_CH4 * GlobalFactors$GWPCH4
-    if(debug) print(paste("effluentEmissions ",effluentEmissions))
-    directN20         <- Feedstock$TKN * GlobalFactors$N20N_to_N20 * 
-        GlobalFactors$GWPN20 * GlobalFactors$AD_Storage_IPCC_EF3 / 1000
-    if(debug) print(paste("directN20 ",directN20))
-    indirectN20       <- GlobalFactors$AD_Storage_IPCC_EF4timesFracGasm *
-        Feedstock$TKN * GlobalFactors$N20N_to_N20 * GlobalFactors$GWPN20 / 1000
-    if(debug) print(paste("indirectN20 ",indirectN20))
-    N20Emissions      <- directN20 + indirectN20
-    if(debug) print(paste("N20Emissions ",N20Emissions))
-    Storage           <- effluentEmissions + N20Emissions
-    if(debug) print(paste("Storage ",Storage))
-    
-    # Step 3: Calculate Land Application  kgCO2e/MT
-    xport           <- 1.5 * XportToField/20
-    if(debug) print(paste("xport ",xport))
-    Nremaining      <- Feedstock$TKN - GlobalFactors$AD_Storage_IPCC_EF3 * Feedstock$TKN -
-        Feedstock$TKN*0.26
-    direct2         <- Nremaining * GlobalFactors$AD_LandApplication_mysteryFactor1 *
-        GlobalFactors$N20N_to_N20 * GlobalFactors$GWPN20 / 1000
-    if(debug) print(paste("direct2 ",direct2))
-    indirect2       <- Nremaining * GlobalFactors$AD_LandApplication_mysteryFactor2 *
-        GlobalFactors$N20N_to_N20 * GlobalFactors$GWPN20 / 1000
-    if(debug) print(paste("indirect2 ",indirect2))
-    N20Emissions    <- direct2 + indirect2
-    if(debug) print(paste("N20Emissions ",N20Emissions))
-    LandApplication <- xport + N20Emissions
-    if(debug) print(paste("LandApplication ",LandApplication))
-    
-    # Step 4: Displaced fertilizer kgCO2e/MT
-    Nremaining      <- Nremaining - 
-        Nremaining * GlobalFactors$AD_LandApplication_mysteryFactor1 -
-        Nremaining * 0.2
-    effectiveNapplied <- Nremaining * 
-        GlobalFactors$AD_DisplacedFertilizer_mineralizationFactor
-    avoidedNfert    <- GlobalFactors$AD_DisplacedFertilizer_WoodCowieEmissionFactor *
-        effectiveNapplied/1000
-    avoidedNfertDirectAndIndirect <- GlobalFactors$AD_DisplacedFertilizer_mysteryFactor1 *
-        effectiveNapplied/1000
-    displacedFertilizer <- avoidedNfert + avoidedNfertDirectAndIndirect
-    if(debug) print(paste("displacedFertilizer ",displacedFertilizer))
-    
-    # Add together
-    netEmissions <- 
-        Digester +
-        Storage +
-        LandApplication +
-        displacedFertilizer
-    names(netEmissions) <- Feedstock$type
-    return(netEmissions)
-}
 
 
 
