4210df7d32ce4b7652ca46477c11bec20003aeef
graySquirrel <fritzebner@gmail.com>
2015-08-05 16:35:26 -0400

    unignore.  added source of treatmentLandApplication.R

---
+++ unitTest.R
@@ -0,0 +1,19 @@
+# simuTreat.R to simulate treatments
+#    of different feedstocks to generate GHG and cost outputs.
+library(rgl)
+#setwd("C:/Users/febner/Documents/CourseraDataScience/fracGASM")
+source("treatmentClasses.R") 
+source("treatmentAnaerobicDigestion.R") 
+source("treatmentLandApplication.R") 
+
+
+f1 <- Feedstock(type="GTW",
+                TS=18,
+                VS=17/18,
+                Bo=887,
+                TKN=5600)
+
+g1 <- GlobalFactors()
+res <- AnaerobicDigestionTreatmentPathway(f1, g1, debug = T)
+
+print(res)
