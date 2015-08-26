42ae2395df2e925c136f12215399ab87bc821a75
graySquirrel <fritzebner@gmail.com>
2015-08-25 20:01:36 -0400

    added 10 steps per year.

--- treatmentLandfill.R
+++ treatmentLandfill.R
@@ -4,6 +4,7 @@
 #       returns data.frame of factor vs. outputs, labeled with row and col names
 #
 ################# Treatment Functions
+library(gtools)
 LandfillTreatmentPathway <- function(Feedstock, GlobalFactors, debug = F)
 {
  
@@ -19,11 +20,23 @@ LandfillTreatmentPathway <- function(Feedstock, GlobalFactors, debug = F)
     # EMTransport <- 
     # Step 1: calculate Digester emissions kgCO2e/MT
     Max_Years=50
-    tcol<-1:Max_Years
+    #tcol<-1:Max_Years
+    tcol<-1:(Max_Years*10)
     #Q is methane produced
-    Q<-Feedstock$Lo*k*exp(-tcol*k)
+    #Q<-Feedstock$Lo*k*exp(-tcol*k)
+    Q10x<-Feedstock$Lo*k*0.1*exp(-tcol*k*0.1)
+    # sum up the 10 intra-year steps before multiplying LCE
+    Q <- running(Q10x, fun=sum, width=10, align="left", by=10)
+    
+    
+    if(debug) {print(paste("Q len ",length(Q)));
+                     print(Q)}
     Landfill_LCE<- c(0,0.5,0.5,0.5,0.5,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,0.75,
-                     0.825,0.825,rep(0.95,Max_Years-16))
+                     0.825,0.825,rep(0.95,Max_Years-17))
+    if(debug) {print(paste("Landfill_LCE len ",length(Landfill_LCE)));
+        print(Landfill_LCE)}
+    
+    
     Percent_Captured<-(Landfill_GC*Landfill_LCE)
     Percent_Released<-(1-Percent_Captured)
     Q_Released<-Q*(1-Landfill_Oxidation_Factor)* Percent_Released
