d4e9c5501f3d05929610474b4c55862f13fa03a5
graySquirrel <fritzebner@gmail.com>
2015-08-31 13:35:35 -0400

    new file.  variation on single dimension sampling makes a hypercube.

---
+++ varyFeedstockAroundNom.R
@@ -0,0 +1,31 @@
+# varyFeedstockAroundNom will vary a feedstock factors systematically to sample
+#   a hypercube over the ranges of interest
+#setwd("C:/Users/febner/Documents/CourseraDataScience/fracGASM")
+source("treatmentClasses.R") 
+source("treatmentAnaerobicDigestion.R") 
+source("treatmentLandApplication.R") 
+###########################################################
+createSamples <- function(dims=4,level=1,samps=10,min=0,max=1) {
+    rep(0:(samps-1),times=samps^(level-1),each=samps^(dims-level))*((max-min)/(samps-1)) + min
+}
+###########################################################
+####  Change these params to do a diff one.
+FUN=AnaerobicDigestionTreatmentPathway
+numsampsPerDimension = 10 # Will create numsampsPerDimension^4 samples for the cube
+
+
+inputs <- data.frame(createSamples(dims=4,level=1,samps=numsampsPerDimension, min=14,max=22))
+inputs <- cbind(inputs, createSamples(dims=4,level=2,samps=numsampsPerDimension, min=13/18, max= 18/18))
+inputs <- cbind(inputs, createSamples(dims=4,level=3,samps=numsampsPerDimension, min=750, max=950))
+inputs <- cbind(inputs, createSamples(dims=4,level=4,samps=numsampsPerDimension, min=4500, max=6500))
+colnames(inputs) <- c("TS","VS","Bo","TKN")
+f1 <- Feedstock(type="variability", TS=inputs[,1], VS=inputs[,2], Bo=inputs[,3], TKN=inputs[,4])
+g1 <- GlobalFactors()
+out <- FUN(f1, g1, debug = F)
+inout <- cbind(inputs,out)
+
+# plot Net Emissions vs. each of the 4 inputs.
+plot(inputs[,1],out[,1])
+plot(inputs[,2],out[,1])
+plot(inputs[,3],out[,1])
+plot(inputs[,4],out[,1])
