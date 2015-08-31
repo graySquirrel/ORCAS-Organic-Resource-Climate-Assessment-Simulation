ab4c0bb4375851a76313bc45dc1a334330a74778
jhe5003 <jhe5003@rit.edu>
2015-08-30 20:19:48 -0400

    got it to compile
    now need to get the numbers right.

--- treatmentcompost.R
+++ treatmentcompost.R
@@ -13,9 +13,19 @@ compostTreatmentPathway <- function(Feedstock, GlobalFactors, debug = F)
   Compost_degradedC_CH4<-0.02
   #Boldrin,2009
   Compost_N2OperN<-0.015
+  Compost_storage_factor<-0.00
+# Additional decay beyond AD degradation tests due to organisms and fungi
+  Compost_mass_reduction=0.4
+  Compost_spread_diesellpert<-0.4
+  Compost_N_remaining<-0.4
+  Compost_NAvailabiltiy_Factor<-0.4
+  xportToField<-60
+  Peatdisplacementfactor<-1
+  EF_Peat_kgCO2eperton<--970
 # Hauling of the waste to the compost facility not included at this time
 # step 1: Compost operation
-  EMCompostoperation<-Compost_dieseLlpert*(GlobalFactors$DieselprovisionkgCO2eperL+GlobalFactors$DieselcombustionkgCO2eperL)
+  EMCompostoperation<-Compost_dieseLlpert*
+    (GlobalFactors$DieselprovisionkgCO2eperL+GlobalFactors$DieselcombustionkgCO2eperL)
   if(debug) {print(paste("EMCompostoperation", (EMCompostoperation)))}
   
 # step 2: Biological emissions
@@ -27,8 +37,43 @@ compostTreatmentPathway <- function(Feedstock, GlobalFactors, debug = F)
    
   
 #Step 3 Carbon storage
-    #CStorage<-InitialC*(1-fdeg)
-    #EMCstorage<-Cstorage*44/12
+    CStorage<-Feedstock$InitialC*(1-(Feedstock$fdeg+Compost_storage_factor))
+    #Assuming that the same amount is stored long term as AD degradability test
+    if(debug) {print(paste("CStorage", (CStorage)))}
+    EMCstorage<-CStorage*-44/12
+    if(debug) {print(paste("EMCstorage", (EMCstorage)))}
+    
+#Step 4 Compost application
+    Compost_mass<- 1000*Compost_mass_reduction
+    EMspread           <- 1.5 * xportToField/20
+    if(debug) print(paste("EMspread ",EMspread))
+    Nremaining<-Feedstock$Nperton*Compost_N_remaining
+    EMN20_LandApp_direct         <- Nremaining * GlobalFactors$LandApplication_EF1 *
+      GlobalFactors$N20N_to_N20 * GlobalFactors$GWPN20 / 1000
+    if(debug) print(paste("EMN20_LandApp_direct ",EMN20_LandApp_direct))
+    EMN20_LandApp_indirect       <- Nremaining * GlobalFactors$LandApplication_FracGasM * 
+      GlobalFactors$IPCC_EF4 * GlobalFactors$N20N_to_N20 * GlobalFactors$GWPN20 / 1000
+    if(debug) print(paste("EMN20_LandApp_indirect ",EMN20_LandApp_indirect))
+    EMN20_LandApp    <- EMN20_LandApp_direct + EMN20_LandApp_indirect
+    if(debug) print(paste("EMN20_LandApp ",EMN20_LandApp))
+    EMLandApp <- EMspread + EMN20_LandApp
+    if(debug) print(paste("EMLandApp ",EMLandApp))
+    
+    # Step 4: Displaced fertilizer kgCO2e/MT
+    Nremaining      <- Nremaining - 
+      Nremaining * GlobalFactors$LandApplication_EF1 -
+      Nremaining * 0.2
+    effectiveNapplied <- Nremaining * 
+      Compost_NAvailabiltiy_Factor
+    avoidedNfert    <- GlobalFactors$LA_DisplacedFertilizer_Production_Factor *
+      effectiveNapplied/1000
+    avoidedInorganicFertdirectandIndirect <- GlobalFactors$LA_DisplacedFertilizer_Direct_Indirect *
+      effectiveNapplied/1000
+    EM_displacedFertilizer <- avoidedNfert + avoidedInorganicFertdirectandIndirect
+    if(debug) print(paste("displacedFertilizer ",EM_displacedFertilizer))
+    
+    # Step 5: Displaced peat kgCO2e/Mt
+   EM_displaced_Peat<-Peatdisplacementfactor*Compost_mass*EF_Peat_kgCO2eperton
     
 }
     
--- unitTestcompost.R
+++ unitTestcompost.R
@@ -8,8 +8,9 @@ source("treatmentcompost.R")
 
 f1 <- Feedstock(type="OFMSW",TS=0.3,VS=0.90,Bo=444,TKN=1000,
                 percentCarboTS = 0.76, percentProteinTS = 0.10, percentLipidTS = 0.11,
-                fdeg = 0.94
+                fdeg = 0.85
                 )
+f1$InitialC<-142.72
 #print(paste("Lo ",f1$Lo, "TVS ",f1$TVS,"InitialC ",f1$InitialC))
 g1 <- GlobalFactors()
 res <- compostTreatmentPathway(f1, g1, debug = T)
