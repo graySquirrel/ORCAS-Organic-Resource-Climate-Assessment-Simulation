500172de97ade3c2292f77f7b4b6a1d354fc435c
graySquirrel <fritzebner@gmail.com>
2015-09-04 15:18:36 -0400

    updated feedstock parameters

--- unitTestLandfill.R
+++ unitTestLandfill.R
@@ -7,12 +7,12 @@ source("treatmentClasses.R")
 source("treatmentLandfill.R") 
 
 
-f1 <- Feedstock(type="OFMSW",TS=0.3,VS=0.90,Bo=444,TKN=5600,
-                percentCarboTS = 0.76, percentProteinTS = 0.10, percentLipidTS = 0.11,
-                fdeg = 0.94
-                )
+f1 <- Feedstock(type="OFMSW",TS=0.3,VS=0.90,Bo=334,TKN=5600,
+                percentCarboTS = 0.8, percentProteinTS = 0.10, percentLipidTS = 0.10,
+                fdeg = 0.85)
+
 print(paste("TS",f1$TS,"Lo ",f1$Lo," TVS ",f1$TVS,"initialC ",f1$InitialC))
 g1 <- GlobalFactors()
-res <- LandfillTreatmentPathway(f1, g1, debug = F)
+res <- LandfillTreatmentPathway(f1, g1, debug = T)
 
 print(res)
