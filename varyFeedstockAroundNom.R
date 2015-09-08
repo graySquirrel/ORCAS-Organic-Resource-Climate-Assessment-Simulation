10cfdf07d803c9e2558ba1aca72c4499e940c40d
graySquirrel <fritzebner@gmail.com>
2015-09-08 16:13:13 -0400

    updated to add bells and whistles

--- varyFeedstockAroundNom.R
+++ varyFeedstockAroundNom.R
@@ -11,20 +11,38 @@ createSamples <- function(dims=4,level=1,samps=10,min=0,max=1) {
     rep(0:(samps-1),times=samps^(level-1),each=samps^(dims-level))*((max-min)/(samps-1)) + min
 }
 ###########################################################
-plotit <- function(a,x,title,xlab) {
-    modelQ1<-lm(a ~ x)
+plotit <- function(a,x,col='red',plotranges=FALSE) {
+    #modelQ1<-lm(a ~ x)
     newx <- seq(min(x), max(x), length.out = 10)
-#    plot(a ~ x,xlab=xlab,ylab=title, type = 'n')
     asum <- boxplot(a ~ x,plot= FALSE)
-    polygon(c(rev(newx), newx), c(rev(asum$stats[5,]), asum$stats[1,]), col = 'grey80', border = NA)
-    polygon(c(rev(newx), newx), c(rev(asum$stats[4,]), asum$stats[2,]), col = 'grey65', border = NA)
-    polygon(c(rev(newx), newx), c(rev(asum$conf[2,]), asum$conf[1,]), col = 'grey50', border = NA)
-    lines(asum$stats[3,]~newx,type='l',lwd=2)
+    if(plotranges) {
+        polygon(c(rev(newx), newx), c(rev(asum$stats[5,]), asum$stats[1,]), col = 'grey95', border = NA)
+        polygon(c(rev(newx), newx), c(rev(asum$stats[4,]), asum$stats[2,]), col = 'grey90', border = NA)
+        # polygon(c(rev(newx), newx), c(rev(asum$conf[2,]), asum$conf[1,]), col = 'grey85', border = NA)
+    }
+    lines(asum$stats[3,]~newx,type='l',lwd=2, col=col)
 }
 ###########################################################
+plotAllOneVar <- function(inVar=NULL,xaxis='no',doleg = FALSE,plotranges=FALSE) {
+    myin<-inVar
+    minlim <- min(outAD[,1],outLF[,1],outCM[,1],outCMf[,1],outCMp[,1])
+    maxlim <- max(outAD[,1],outLF[,1],outCM[,1],outCMf[,1],outCMp[,1])
+    plot(outAD[,1] ~ myin,xlab=xaxis,ylab='Net Emissions', type = 'n',ylim=c(minlim, maxlim))
+    plotit(outAD[,1], myin,'red',plotranges)
+    plotit(outLF[,1], myin,'green3',plotranges)
+    plotit(outCM[,1], myin,'black',plotranges)
+    plotit(outCMf[,1], myin,'cyan',plotranges)
+    plotit(outCMp[,1], myin,'blue',plotranges)
+    print(maxlim)
+    if(doleg)
+        legend(0.05, maxlim*0.95, c("AD","LF","CM","CMf","CMp"),
+               lty=c(1,1,1,1,1),
+               lwd=c(2.5,2.5,2.5,2.5,2.5),
+               col=c('red','green3','black','cyan','blue'))
+}
 ####  Change these params to do a diff one.
 numsampsPerDimension = 10 # Will create numsampsPerDimension^4 samples for the cube
-inputs <- data.frame(createSamples(dims=4,level=1,samps=numsampsPerDimension, min=0,max=1))
+inputs <- data.frame(createSamples(dims=4,level=1,samps=numsampsPerDimension, min=0.038,max=0.937))
 inputs <- cbind(inputs, createSamples(dims=4,level=2,samps=numsampsPerDimension, min=0.8, max= 1))
 inputs <- cbind(inputs, createSamples(dims=4,level=3,samps=numsampsPerDimension, min=180, max=1100))
 inputs <- cbind(inputs, createSamples(dims=4,level=4,samps=numsampsPerDimension, min=3700, max=13000))
@@ -39,33 +57,15 @@ outCMf <- compostTreatmentPathway(f1, g1, 'Fertilizer', debug = F)
 outCMp <- compostTreatmentPathway(f1, g1, 'Peat', debug = F)
 
 if(dev.cur() != 1) dev.off() 
-#par(mfrow=c(2,2))
-myin<-inputs$TS
-minlim <- min(outAD[,1],outLF[,1],outCM[,1],outCMf[,1],outCMp[,1])
-maxlim <- max(outAD[,1],outLF[,1],outCM[,1],outCMf[,1],outCMp[,1])
-plot(outAD[,1] ~ myin,xlab='TS',ylab='Net Emissions', type = 'n',ylim=c(minlim, maxlim))
+par(mfrow=c(2,2))
+plotAllOneVar(inputs$TS,xaxis='TS',doleg=TRUE)
+plotAllOneVar(inputs$VS,xaxis='VS')
+plotAllOneVar(inputs$Bo,xaxis='Bo')
+plotAllOneVar(inputs$TKN,xaxis='TKN')
 
-plotit(outAD[,1], myin, "AD Net Emissions", "TS")
-plotit(outLF[,1], myin, "Landfill Net Emissions", "TS")
-plotit(outCM[,1], myin, "Compost Net Emissions", "TS")
-plotit(outCMf[,1], myin, "Compost(fert) Net Emissions", "TS")
-plotit(outCMp[,1], myin, "Compost(Peat) Net Emissions", "TS")
+par(mfrow=c(2,2))
+plotAllOneVar(inputs$TS,xaxis='TS',doleg=TRUE,plotranges=TRUE)
+plotAllOneVar(inputs$VS,xaxis='VS',plotranges=TRUE)
+plotAllOneVar(inputs$Bo,xaxis='Bo',plotranges=TRUE)
+plotAllOneVar(inputs$TKN,xaxis='TKN',plotranges=TRUE)
 
-# myin<-inputs$VS
-# plotit(outAD[,1], myin, "AD Net Emissions", "VS")
-# plotit(outLF[,1], myin, "Landfill Net Emissions", "VS")
-# plotit(outCMf[,1], myin, "Compost(fert) Net Emissions", "VS")
-# plotit(outCMp[,1], myin, "Compost(Peat) Net Emissions", "VS")
-# myin<-inputs$Bo
-# plotit(outAD[,1], myin, "AD Net Emissions", "Bo")
-# plotit(outLF[,1], myin, "Landfill Net Emissions", "Bo")
-# plotit(outCMf[,1], myin, "Compost(fert) Net Emissions", "Bo")
-# plotit(outCMp[,1], myin, "Compost(Peat) Net Emissions", "Bo")
-# myin<-inputs$TKN
-# plotit(outAD[,1], myin, "AD Net Emissions", "TKN")
-# plotit(outLF[,1], myin, "Landfill Net Emissions", "TKN")
-# plotit(outCMf[,1], myin, "Compost(fert) Net Emissions", "TKN")
-# plotit(outCMp[,1], myin, "Compost(Peat) Net Emissions", "TKN")
-# # plotit(outAD[,1], inputs$VS, "VS")
-# # plotit(outAD[,1], inputs$Bo, "Bo")
-# # plotit(outAD[,1], inputs$TKN, "TKN")
