a306773b34fd819aa98253b3fa19e938850c86f6
graySquirrel <fritzebner@gmail.com>
2015-09-09 16:27:08 -0400

    fixed oops.

--- baselines.R
+++ baselines.R
@@ -15,20 +15,20 @@ f1 <- Feedstock(type=i$Feedstock,TS=i$TS,VS=i$VS,Bo=i$Bo,TKN=i$TKN,
 #                 fdeg = 0.85)
 g1 <- GlobalFactors()
 o<-NULL
-o$ADNoDisplace <- AnaerobicDigestionTreatmentPathway(f1, g1, Application = 'noDisplace')
-o$ADFertDisplace <- AnaerobicDigestionTreatmentPathway(f1, g1, Application = 'Fertilizer')
-o$LANoDisplace <- LandApplicationTreatmentPathway(f1, g1, Ninitial = f1$TKN, 
+o$AD <- AnaerobicDigestionTreatmentPathway(f1, g1, Application = 'noDisplace')
+o$ADf <- AnaerobicDigestionTreatmentPathway(f1, g1, Application = 'Fertilizer')
+o$LA <- LandApplicationTreatmentPathway(f1, g1, Ninitial = f1$TKN, 
                                                 Application = 'noDisplace')
-o$LAFertDisplace <- LandApplicationTreatmentPathway(f1, g1, Ninitial = f1$TKN, 
+o$LAf <- LandApplicationTreatmentPathway(f1, g1, Ninitial = f1$TKN, 
                                                   Application = 'Fertilizer')
-o$CMNoDisplace <- compostTreatmentPathway(f1, g1, Application = 'noDisplace')
-o$CMFertDisplace <- compostTreatmentPathway(f1, g1, Application = 'Fertilizer')
-o$CMPeatDisplace <- compostTreatmentPathway(f1, g1, Application = 'Peat')
-o$Landfill <- LandfillTreatmentPathway(f1, g1)
+o$CM <- compostTreatmentPathway(f1, g1, Application = 'noDisplace')
+o$CMf <- compostTreatmentPathway(f1, g1, Application = 'Fertilizer')
+o$CMp <- compostTreatmentPathway(f1, g1, Application = 'Peat')
+o$LF <- LandfillTreatmentPathway(f1, g1)
 
-result <- data.frame(f1$type,ADNoDisplace[[1]],ADFertDisplace[[1]],
-                     LANoDisplace[[1]],LAFertDisplace[[1]],CMNoDisplace[[1]],
-                     CMFertDisplace[[1]],CMPeatDisplace[[1]],Landfill[[1]],
+result <- data.frame(f1$type,o$AD[[1]],o$ADf[[1]],
+                     o$LA[[1]],o$LAf[[1]],o$CM[[1]],
+                     o$CMf[[1]],o$CMp[[1]],o$LF[[1]],
                      f1$TS,f1$VS,f1$Bo,f1$TKN,f1$percentCarboTS,f1$percentLipidTS,
                      f1$percentProteinTS,f1$Lo,f1$TVS,f1$fdeg)
 colnames(result) <- c("Feedstock Type","AD no displacement", 
@@ -40,5 +40,4 @@ colnames(result) <- c("Feedstock Type","AD no displacement",
                       "percentCarboTS","percentLipidTS","percentProteinTS","Lo","TVS","fdeg")
 
 write.csv(result, file="baselinesOutput.csv",row.names=FALSE)
-justFoodWaste <- lapply(o, function(i) head(i, n=1)) # to check food waste outputs.
 write.csv(o, file="allPathwaysBreakdowns.csv",row.names=f1$type)
