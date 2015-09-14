0eacff92f57cf435436cb89a308f3716ac9924ce
graySquirrel <fritzebner@gmail.com>
2015-09-14 14:29:51 -0400

    added plotting of output using ggplot2

--- doMonteCarloAnalyses.R
+++ doMonteCarloAnalyses.R
@@ -40,7 +40,40 @@ calculatePathwayMC <- function(feedstockfile="Feedstock.csv",
     o$outRanges <- outRanges
     o
 }
+#####################################################################
 ADstats <- calculatePathwayMC(FUN=AnaerobicDigestionTreatmentPathway)
 LFstats <- calculatePathwayMC(FUN=LandfillTreatmentPathway)
 CMstats <- calculatePathwayMC(FUN=compostTreatmentPathway)
+#####################################################################
+# Now do the dirty work of plotting
+library(ggplot2)
+x1 <- t(ADstats$confDat)
+y1<-data.frame(x1)
+y1 <- cbind(rownames(y1),y1)
+colnames(y1)<- c("feedstock","lo","Emissions","hi")
+y1$treatment <- c("AD")
+y1$feedstock <- factor(y1$feedstock)
+
+x2 <- t(LFstats$confDat)
+y2 <- data.frame(x2)
+y2 <- cbind(rownames(y2),y2)
+colnames(y2) <- c("feedstock","lo","Emissions","hi")
+y2$treatment <- c("LF")
+y2$feedstock <- factor(y2$feedstock)
+
+x3 <- t(CMstats$confDat)
+y3 <- data.frame(x3)
+y3 <- cbind(rownames(y3),y3)
+colnames(y3) <- c("feedstock","lo","Emissions","hi")
+y3$treatment <- c("CM")
+y3$feedstock <- factor(y3$feedstock)
+
+y <- rbind(y1,y2,y3)
+y$feedstock <- factor(y$feedstock, levels=y$feedstock[order(y2$Emissions)]) # order by LF
+
+ggplot(y, aes(x=feedstock, y=Emissions,fill=treatment)) + 
+    geom_bar(position=position_dodge(), stat="identity") +
+    geom_errorbar(aes(ymin=lo, ymax=hi), width=.3, position=position_dodge(0.9)) +
+    theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5,size=16))
+
 
