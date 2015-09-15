acdf7842b01e0fed0367c486150bd509e8adef1c
jhe5003 <jhe5003@rit.edu>
2015-09-15 17:49:49 -0400

    Changed fdeg slightly based on Barlaz paper

--- unitTest.R
+++ unitTest.R
@@ -9,7 +9,7 @@ source("treatmentLandApplication.R")
 
 f1 <- Feedstock(type="OFMSW",TS=0.3,VS=0.90,Bo=334,TKN=5600,
                 percentCarboTS = 0.8, percentProteinTS = 0.10, percentLipidTS = 0.10,
-                fdeg = 0.85)
+                fdeg = 0.84.1)
 
 g1 <- GlobalFactors()
 res <- AnaerobicDigestionTreatmentPathway(f1, g1, debug = T, Application = 'noDisplace')
