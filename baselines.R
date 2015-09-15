b479bf2116a97d1d5a80a72091c1ab808fd43345
graySquirrel <fritzebner@gmail.com>
2015-09-15 06:22:24 -0400

    put it in a function so i can call it from other places.

--- baselines.R
+++ baselines.R
@@ -7,21 +7,25 @@ source("treatmentLandfill.R")
 source("treatmentcompost.R")
 source("parseGlobalFactors.R")
 
-i <- read.csv(file="Feedstock.csv",sep = ",",stringsAsFactors=FALSE)
-f1 <- Feedstock(type=i$Feedstock,TS=i$TS,VS=i$VS,Bo=i$Bo,TKN=i$TKN,
-                percentCarboTS = i$PercentCarboTS, percentLipidTS = i$PercentlipidTS,
-                percentProteinTS = i$PercentproteinTS, fdeg = i$fdeg)
-g1 <- getGlobalFactorsFromFile(doRanges = FALSE)
-o<-NULL
-o$AD <- AnaerobicDigestionTreatmentPathway(f1, g1, Application = 'noDisplace')
-o$ADf <- AnaerobicDigestionTreatmentPathway(f1, g1, Application = 'Fertilizer')
-o$LA <- LandApplicationTreatmentPathway(f1, g1, Application = 'noDisplace')
-o$LAf <- LandApplicationTreatmentPathway(f1, g1, Application = 'Fertilizer')
-o$CM <- compostTreatmentPathway(f1, g1, Application = 'noDisplace')
-o$CMf <- compostTreatmentPathway(f1, g1, Application = 'Fertilizer')
-o$CMp <- compostTreatmentPathway(f1, g1, Application = 'Peat')
-o$LF <- LandfillTreatmentPathway(f1, g1)
+getBaselineResults <- function() {
+    i <- read.csv(file="Feedstock.csv",sep = ",",stringsAsFactors=FALSE)
+    f1 <- Feedstock(type=i$Feedstock,TS=i$TS,VS=i$VS,Bo=i$Bo,TKN=i$TKN,
+                    percentCarboTS = i$PercentCarboTS, percentLipidTS = i$PercentlipidTS,
+                    percentProteinTS = i$PercentproteinTS, fdeg = i$fdeg)
+    g1 <- getGlobalFactorsFromFile(doRanges = FALSE)
+    o<-NULL
+    o$AD <- AnaerobicDigestionTreatmentPathway(f1, g1, Application = 'noDisplace')
+    o$ADf <- AnaerobicDigestionTreatmentPathway(f1, g1, Application = 'Fertilizer')
+    o$LA <- LandApplicationTreatmentPathway(f1, g1, Application = 'noDisplace')
+    o$LAf <- LandApplicationTreatmentPathway(f1, g1, Application = 'Fertilizer')
+    o$CM <- compostTreatmentPathway(f1, g1, Application = 'noDisplace')
+    o$CMf <- compostTreatmentPathway(f1, g1, Application = 'Fertilizer')
+    o$CMp <- compostTreatmentPathway(f1, g1, Application = 'Peat')
+    o$LF <- LandfillTreatmentPathway(f1, g1)
+    o
+}
 
+o <- getBaselineResults()
 result <- data.frame(f1$type,o$AD[[1]],o$ADf[[1]],
                      o$LA[[1]],o$LAf[[1]],o$CM[[1]],
                      o$CMf[[1]],o$CMp[[1]],o$LF[[1]],
