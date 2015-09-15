5bc56f32b672e6da90c52eb182de37ec34e4032d
graySquirrel <fritzebner@gmail.com>
2015-09-15 06:45:29 -0400

    refactored for ploting.  now plotting median and also nominal

--- doMonteCarloAnalyses.R
+++ doMonteCarloAnalyses.R
@@ -9,6 +9,7 @@ source("treatmentLandApplication.R")
 source("treatmentcompost.R")
 source("treatmentLandfill.R")
 source("parseGlobalFactors.R")
+source("baselines.R")
 #################################################################################
 calculatePathwayMC <- function(feedstockfile="Feedstock.csv",
                              factorFile="Globalfactors.csv",
@@ -44,36 +45,39 @@ calculatePathwayMC <- function(feedstockfile="Feedstock.csv",
 ADstats <- calculatePathwayMC(FUN=AnaerobicDigestionTreatmentPathway)
 LFstats <- calculatePathwayMC(FUN=LandfillTreatmentPathway)
 CMstats <- calculatePathwayMC(FUN=compostTreatmentPathway)
+LAstats <- calculatePathwayMC(FUN=LandApplicationTreatmentPathway)
 #####################################################################
+# Get baselines
+b <- getBaselineResults()
+#####################################################################
+massageDataforPlot <- function(in1,in2,treat) {
+    df <-data.frame(t(in1))
+    df <- cbind(rownames(df),df,in2)
+    colnames(df)<- c("feedstock","lo","Median","hi","Nominal")
+    df$treatment <- treat
+    df$feedstock <- factor(df$feedstock)
+    df
+}
 # Now do the dirty work of plotting
 library(ggplot2)
-x1 <- t(ADstats$confDat)
-y1<-data.frame(x1)
-y1 <- cbind(rownames(y1),y1)
-colnames(y1)<- c("feedstock","lo","Emissions","hi")
-y1$treatment <- c("AD")
-y1$feedstock <- factor(y1$feedstock)
 
-x2 <- t(LFstats$confDat)
-y2 <- data.frame(x2)
-y2 <- cbind(rownames(y2),y2)
-colnames(y2) <- c("feedstock","lo","Emissions","hi")
-y2$treatment <- c("LF")
-y2$feedstock <- factor(y2$feedstock)
+y1 <- massageDataforPlot(ADstats$confDat, b$AD$ADnetEmissions,"AD")
+y2 <- massageDataforPlot(LFstats$confDat, b$LF$LandfillNetEmissions,"LF")
+y3 <- massageDataforPlot(CMstats$confDat, b$CM$final,"CM")
+y4 <- massageDataforPlot(LAstats$confDat, b$LA$EMNetLandapp,"LA")
 
-x3 <- t(CMstats$confDat)
-y3 <- data.frame(x3)
-y3 <- cbind(rownames(y3),y3)
-colnames(y3) <- c("feedstock","lo","Emissions","hi")
-y3$treatment <- c("CM")
-y3$feedstock <- factor(y3$feedstock)
+y <- rbind(y1,y2,y3,y4)
+y$feedstock <- factor(y$feedstock, levels=y$feedstock[order(y2$Nominal)]) # order by LF
 
-y <- rbind(y1,y2,y3)
-y$feedstock <- factor(y$feedstock, levels=y$feedstock[order(y2$Emissions)]) # order by LF
-
-ggplot(y, aes(x=feedstock, y=Emissions,fill=treatment)) + 
+# Plot Nominal values
+ggplot(y, aes(x=feedstock, y=Nominal,fill=treatment)) + 
     geom_bar(position=position_dodge(), stat="identity") +
     geom_errorbar(aes(ymin=lo, ymax=hi), width=.3, position=position_dodge(0.9)) +
     theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5,size=16))
 
+# Make a second plot to show median values
+ggplot(y, aes(x=feedstock, y=Median,fill=treatment)) + 
+    geom_bar(position=position_dodge(), stat="identity") +
+    geom_errorbar(aes(ymin=lo, ymax=hi), width=.3, position=position_dodge(0.9)) +
+    theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5,size=16))
 
