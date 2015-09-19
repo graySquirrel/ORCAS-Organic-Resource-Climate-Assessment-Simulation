3c7ed15ba41839165302dd2ed370c22dee17db50
jhe5003 <jhe5003@rit.edu>
2015-09-18 20:20:37 -0400

    added indirect emissions and fixed carbon storage

--- treatmentcompost.R
+++ treatmentcompost.R
@@ -9,69 +9,58 @@
 ################# Treatment Functions
 compostTreatmentPathway <- function(Feedstock, GlobalFactors, Application = 'noDisplace', debug = F)
 {
-  Compost_EF1 <- GlobalFactors$Compost_EF1
-  Compost_dieseLlpert <- GlobalFactors$Compost_dieseLlpert
-  #Boldrin,2009
-  CompostPercentCdegraded <- GlobalFactors$CompostPercentCdegraded
-  #Boldrin,2009
-  Compost_degradedC_CH4 <- GlobalFactors$Compost_degradedC_CH4
-  #Boldrin,2009
-  Compost_N2OperN <- GlobalFactors$Compost_N2OperN
-  Compost_storage_factor <- GlobalFactors$Compost_storage_factor
-# Additional decay beyond AD degradation tests due to organisms and fungi
-  Compost_mass_reduction <- GlobalFactors$Compost_mass_reduction
-  Compost_spread_diesellpert <- GlobalFactors$Compost_spread_diesellpert
-  Compost_N_remaining <- GlobalFactors$Compost_N_remaining
-  Compost_NAvailabiltiy_Factor <- GlobalFactors$Compost_NAvailabiltiy_Factor
-  Compost_xportToField <- GlobalFactors$Compost_xportToField
-  Peatdisplacementfactor <- GlobalFactors$Peatdisplacementfactor
-  EF_Peat_kgCO2eperton <- GlobalFactors$EF_Peat_kgCO2eperton
+ 
 # Hauling of the waste to the compost facility not included at this time
 # step 1: Compost operation
-  EMCompostoperation<-Compost_dieseLlpert*
+  EMCompostoperation<-GlobalFactors$Compost_dieseLlpert*
     (GlobalFactors$DieselprovisionkgCO2eperL+GlobalFactors$DieselcombustionkgCO2eperL)
   if(debug) {print(paste("EMCompostoperation", (EMCompostoperation)))}
   
 # step 2: Biological emissions
-   EMCompost_CH4<-CompostPercentCdegraded*Compost_degradedC_CH4*Feedstock$InitialC*
+  #From Boldrin, 2009 based upon percentage of degraded C 
+   EMCompost_CH4<-GlobalFactors$CompostPercentCdegraded*GlobalFactors$Compost_degradedC_CH4*Feedstock$InitialC*
      GlobalFactors$CtoCH4*GlobalFactors$GWPCH4
-   if(debug) {print(paste("Compost_degradedC_CH4", Compost_degradedC_CH4))}
+   if(debug) {print(paste("Compost_degradedC_CH4",GlobalFactors$Compost_degradedC_CH4))}
    
-   EMCompost_N2O=Feedstock$Nperton*Compost_N2OperN*GlobalFactors$N20N_to_N20*
+   #From Boldrin, 2009, based upon N content
+   EMCompost_N2O=Feedstock$Nperton*GlobalFactors$Compost_N2OperN*GlobalFactors$N20N_to_N20*
      GlobalFactors$GWPN20
+   if(debug) {print(paste("GlobalFactors$Compost_N2OperN", (GlobalFactors$Compost_N2OperN),"Nperton",(Feedstock$Nperton)))}
+   EMBio <- EMCompost_N2O + EMCompost_CH4
    if(debug) {print(paste("EMCompost_CH4", (EMCompost_CH4),"EMCompost_N2O",(EMCompost_N2O)))}
    EMBio <- EMCompost_N2O + EMCompost_CH4
   
 #Step 3 Carbon storage
-    CStorage<-Feedstock$InitialC * (1-Feedstock$fdeg) * (1-Compost_storage_factor)
+    CStorage<-Feedstock$InitialC * (1-Feedstock$fdeg) * (1-GlobalFactors$Compost_CS_factor)
     #Assuming that the same amount is stored long term as AD degradability test
     if(debug) {print(paste("CStorage", (CStorage)))}
     EMCstorage<-CStorage * -44/12
     if(debug) {print(paste("EMCstorage", (EMCstorage)))}
     
 #Step 4 Compost application
-    Compost_mass<- 1000*Compost_mass_reduction
-    EMspread           <- 1.5 * Compost_xportToField/20
+    Compost_mass<- 1000*GlobalFactors$Compost_mass_reduction
+    EMspread           <- 1.5 * GlobalFactors$Compost_xportToField/20
     if(debug) print(paste("EMspread ",EMspread))
-    Nremaining<-Feedstock$Nperton*Compost_N_remaining
+    
+    Nremaining<-Feedstock$Nperton*(1-GlobalFactors$Compost_N_loss)
     if(debug) print(paste("Nremaining 1 ",Nremaining))
     
-    EMN20_LandApp_direct         <- Nremaining * Compost_EF1 *
+    EMN20_CompApp_direct         <- Nremaining * GlobalFactors$Compost_EF1 *
       GlobalFactors$N20N_to_N20 * GlobalFactors$GWPN20
-    if(debug) print(paste("EMN20_LandApp_direct ",EMN20_LandApp_direct))
-    #EMN20_LandApp_indirect       <- Nremaining * GlobalFactors$LandApplication_FracGasM * 
-    #  GlobalFactors$IPCC_EF4 * GlobalFactors$N20N_to_N20 * GlobalFactors$GWPN20 / 1000
-    #if(debug) print(paste("EMN20_LandApp_indirect ",EMN20_LandApp_indirect))
-    EMN20_LandApp    <- EMN20_LandApp_direct # + EMN20_LandApp_indirect
-    if(debug) print(paste("EMN20_LandApp ",EMN20_LandApp))
-    EMLandApp <- EMspread + EMN20_LandApp
-    if(debug) print(paste("EMLandApp ",EMLandApp))
+    if(debug) print(paste("EMN20_CompApp_direct ",EMN20_CompApp_direct))
+    EMN20_CompApp_indirect       <- Nremaining * GlobalFactors$Compost_FracGasC * 
+     GlobalFactors$IPCC_EF4 * GlobalFactors$N20N_to_N20 * GlobalFactors$GWPN20 / 1000
+    if(debug) print(paste("EMN20_CompApp_indirect ",EMN20_CompApp_indirect))
+    EMN20_CompApp    <- EMN20_CompApp_direct  + EMN20_CompApp_indirect
+    if(debug) print(paste("EMN20_CompApp ",EMN20_CompApp))
+    EMCompApp <- EMspread + EMN20_CompApp
+    if(debug) print(paste("EMCompApp ",EMCompApp))
     
-    EMCompost <- EMCompostoperation + EMBio + EMCstorage + EMLandApp
+    EMCompost <- EMCompostoperation + EMBio + EMCstorage + EMCompApp
     
     # Step 5: Displaced fertilizer kgCO2e/MT
     effectiveNapplied <- Nremaining * 
-      Compost_NAvailabiltiy_Factor
+      GlobalFactors$Compost_NAvailabiltiy_Factor
     avoidedNfert    <- GlobalFactors$LA_DisplacedFertilizer_Production_Factor *
       effectiveNapplied
     avoidedInorganicFertdirectandIndirect <- 
@@ -81,14 +70,14 @@ compostTreatmentPathway <- function(Feedstock, GlobalFactors, Application = 'noD
     if(debug) print(paste("displacedFertilizer ",EM_displacedFertilizer))
     
     # Step 6: Displaced peat kgCO2e/Mt
-   EM_displaced_Peat<-Peatdisplacementfactor*Compost_mass*EF_Peat_kgCO2eperton/1000
+   EM_displaced_Peat<-GlobalFactors$Peatdisplacementfactor*Compost_mass*GlobalFactors$EF_Peat_kgCO2eperton/1000
     
    final <- switch(Application,
           'noDisplace' = EMCompost,
           'Fertilizer' = EMCompost + EM_displacedFertilizer,
           'Peat' = EMCompost + EM_displaced_Peat)
    result <- data.frame(final, Application, EMCompost, EMCompostoperation, 
-                        EMBio, EMCstorage, EMLandApp, EM_displaced_Peat, 
+                        EMBio, EMCstorage, EMCompApp, EM_displaced_Peat, 
                         EM_displacedFertilizer)
 }
     
