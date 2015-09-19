dba8f36f0ac3db294f499d464597f8403b03d7a2
graySquirrel <fritzebner@gmail.com>
2015-09-19 08:45:58 -0400

    removed local assigns, using globalfactors$ everywhere.

--- treatmentLandApplication.R
+++ treatmentLandApplication.R
@@ -11,23 +11,14 @@ LandApplicationTreatmentPathway <- function(Feedstock, GlobalFactors,
                                             debug = F, Nremaining = Feedstock$TKN,
                                             Application = 'noDisplace')
 {
-
-  LandApplication_EF1 = GlobalFactors$LandApplication_EF1
-  #LandApplication_OtherNFactor = GlobalFactors$LandApplication_OtherNFactor
-  LandApp_NAvailabiltiy_Factor = GlobalFactors$LandApp_NAvailabiltiy_Factor
-  LA_DisplacedFertilizer_Production_Factor = 
-      GlobalFactors$LA_DisplacedFertilizer_Production_Factor
-  LA_DisplacedFertilizer_Direct_Indirect = 
-      GlobalFactors$LA_DisplacedFertilizer_Direct_Indirect
-  xportToField = GlobalFactors$xportToField
-
     # Step 1: Calculate Land Application  kgCO2e/MT
-    EMspread           <- 1.5 * xportToField/20
+    EMspread           <- 1.5 * GlobalFactors$xportToField/20
     if(debug) print(paste("EMspread ",EMspread))
-    EMN20_LandApp_direct         <- Nremaining * LandApplication_EF1 *
+    EMN20_LandApp_direct         <- Nremaining * GlobalFactors$LandApplication_EF1 *
         GlobalFactors$N20N_to_N20 * GlobalFactors$GWPN20 / 1000
     if(debug) print(paste("EMN20_LandApp_direct ",EMN20_LandApp_direct))
-    EMN20_LandApp_indirect       <- Nremaining * GlobalFactors$LandApplication_FracGasM * 
+    EMN20_LandApp_indirect       <- Nremaining * 
+        GlobalFactors$LandApplication_FracGasM * 
         GlobalFactors$IPCC_EF4 *
         GlobalFactors$N20N_to_N20 * GlobalFactors$GWPN20 / 1000
     if(debug) print(paste("EMN20_LandApp_indirect ",EMN20_LandApp_indirect))
@@ -40,19 +31,21 @@ LandApplicationTreatmentPathway <- function(Feedstock, GlobalFactors,
     CStorage<-Feedstock$InitialC*(1-Feedstock$fdeg)*(1-GlobalFactors$LA_CSfactor)
     
     EMCstorage<-CStorage*(-44/12)
+    if(debug) print(paste("EMCstorage ",EMCstorage))
     
     # Step 5: Displaced fertilizer kgCO2e/MT
     Nremaining      <- Nremaining - 
-        Nremaining * LandApplication_EF1 -
+        Nremaining * GlobalFactors$LandApplication_EF1 -
         Nremaining * 0.02 - Nremaining * 0.2
-    effectiveNapplied <- Nremaining * LandApp_NAvailabiltiy_Factor
+    effectiveNapplied <- Nremaining * GlobalFactors$LandApp_NAvailabiltiy_Factor
     if(debug) print(paste("effectiveNapplied ",effectiveNapplied))
     
-    avoidedNfert    <- LA_DisplacedFertilizer_Production_Factor *
+    avoidedNfert    <- GlobalFactors$LA_DisplacedFertilizer_Production_Factor *
         effectiveNapplied/1000
     if(debug) print(paste("avoidedNfert ",avoidedNfert))
     
-    avoidedInorganicFertdirectandIndirect <- LA_DisplacedFertilizer_Direct_Indirect *
+    avoidedInorganicFertdirectandIndirect <- 
+        GlobalFactors$LA_DisplacedFertilizer_Direct_Indirect *
         effectiveNapplied/1000
     if(debug) print(paste("avoidedInorganicFertdirectandIndirect ",
                           avoidedInorganicFertdirectandIndirect))
