7348e797e4a4a9baeac905372bf580af591f3423
graySquirrel <fritzebner@gmail.com>
2015-09-19 08:45:31 -0400

    LA_CSfactor was not set

--- treatmentClasses.R
+++ treatmentClasses.R
@@ -101,8 +101,9 @@ GlobalFactors <- function()
         #LandApplication_OtherNFactor = 0.02,
         LandApp_NAvailabiltiy_Factor = 0.4,
         LA_DisplacedFertilizer_Production_Factor = -6.8,
-        LA_DisplacedFertilizer_Direct_Indirect = -5.4)
-        LA_CSfactor = 
+        LA_DisplacedFertilizer_Direct_Indirect = -5.4,
+        LA_CSfactor = 0.8
+    )
     class(me) <- append(class(me), "GlobalFactors")
     return(me)
 }
