13b7c0196d9128d78813d3c166676de126d565c1
graySquirrel <fritzebner@gmail.com>
2015-09-28 23:19:01 -0400

    updated x y axes

--- plotFSparams.R
+++ plotFSparams.R
@@ -10,12 +10,12 @@ source("baselines.R")
 plotplustext <- function(labels,path,x,y,xl,yl) {
     symbols(x=x, y=y, circles=path, 
             inches=1/3, bg=rgb(0, 0, 1, 0.5), xlab=xl,ylab=yl)
-    text(x=x, y=jitter(y-.02,factor=100),labels = labels,cex=0.75)
+    text(x=x, y=jitter(y-.02,factor=10),labels = labels,cex=0.75)
 }
 makeplots <- function(zsize,tit){
     par(mfrow=c(2,2),oma=c(0,0,2,0))
-    plotplustext(labels=o$f1$type,path=zsize,x=o$f1$Bo/500,y=o$f1$TS,xl="Bo/500",yl="TS")
-    plotplustext(labels=o$f1$type,path=zsize,x=o$f1$VS,y=o$f1$TS,xl="VS",yl="TS")
+    plotplustext(labels=o$f1$type,path=zsize,x=o$f1$InitialC,y=o$f1$fdeg,xl="InitialC",yl="fdeg")
+    plotplustext(labels=o$f1$type,path=zsize,x=o$f1$fdeg,y=o$f1$Lo,xl="fdeg",yl="Lo")
     plotplustext(labels=o$f1$type,path=zsize,x=o$f1$TKN/20000,y=o$f1$TS,xl="TKN/20000",yl="TS")
     plotplustext(labels=o$f1$type,path=zsize,x=o$f1$VS,y=o$f1$Bo/500,xl="VS",yl="Bo/500")
     title(tit, outer=TRUE)
