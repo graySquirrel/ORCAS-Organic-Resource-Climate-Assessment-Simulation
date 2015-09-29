5dfaf19a620c610f6684f07cc0ceadde78fbdc75
graySquirrel <fritzebner@gmail.com>
2015-09-28 21:52:01 -0400

    new pathway for Animal Feed

---
+++ treatmentAnimalFeed.R
@@ -0,0 +1,13 @@
+# Animal Feed pathway.
+# Functions
+#   AnimalFeedTreatmentPathway(Feedstock, GlobalFactors, debug = F)
+#       returns data.frame of factor vs. outputs, labeled with row and col names
+#
+############################################################################3
+AnimalFeedTreatmentPathway <- function(Feedstock, GlobalFactors, debug = F)
+{
+    AF_DisplacementFactor <- (Feedstock$TS * Feedstock$TDN) /
+        (GlobalFactors$CornTS * GlobalFactors$CornTDN)
+    if (debug) print(paste("AF_DisplacementFactor", AF_DisplacementFactor))
+    EMAnimalFeed <- AF_DisplacementFactor * GlobalFactors$EFCornDisplacement
+}
