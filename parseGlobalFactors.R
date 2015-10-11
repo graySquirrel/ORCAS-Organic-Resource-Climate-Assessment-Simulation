da5673bc769c13a0a4b41d5f2baace6dfa2d6cba
graySquirrel <fritzebner@gmail.com>
2015-10-11 11:37:45 -0400

    addd a verbose arg to suppress printing of read-in global factors

--- parseGlobalFactors.R
+++ parseGlobalFactors.R
@@ -2,7 +2,9 @@
 # maybe later we will add it as a function
 #
 source("treatmentClasses.R") 
-getGlobalFactorsFromFile <- function(file = "Globalfactors.csv",doRanges=TRUE) {
+getGlobalFactorsFromFile <- function(file = "Globalfactors.csv",
+                                     doRanges=TRUE,
+                                     verbose=FALSE) {
     r <- read.csv(file=file,stringsAsFactors = FALSE)
     # We create a new global factors object to match names
     g1 <- GlobalFactors()
@@ -22,26 +24,28 @@ getGlobalFactorsFromFile <- function(file = "Globalfactors.csv",doRanges=TRUE) {
             if (newUse == "Range" && (is.na(newRangeLo) || is.na(newRangeHigh)))
                 stop(paste("Bad Range (lo or high) for",currentName))
             switch (newUse,
-                    "Value" = {print(paste("setting ",currentName, "from",
+                    "Value" = {
+                        if(verbose) print(paste("setting ",currentName, "from",
                                            currentValue, "to", newValue))
-                        g1[[currentName]] <- newValue},
+                        g1[[currentName]] <- newValue
+                        },
                     "Range" = {
                         if (doRanges) {
-                            print(paste("ranging ",currentName," to ",
+                            if(verbose) print(paste("ranging ",currentName," to ",
                                         numSamps,"samples between",
                                         r[[i,"Range.Lo"]], "and", r[[i,"Range.High"]]))
                             g1[[r$sw.name[i]]] <- runif(n = numSamps,
                                                         min = r[[i,"Range.Lo"]],
                                                         max = r[[i,"Range.High"]])}
                         else {
-                            print(paste("setting ",currentName, "from",
+                            if(verbose) print(paste("setting ",currentName, "from",
                                         currentValue, "to", newValue))
                             g1[[currentName]] <- newValue
                         }},
-                    {print(paste("!no action on",currentName))}
+                    {if(verbose) print(paste("!no action on",currentName))}
             )
         } else {
-            print(paste("!No Variable match for:", r$Global.Factors[i]))
+            if(verbose) print(paste("!No Variable match for:", r$Global.Factors[i]))
         }
     }
     g1
