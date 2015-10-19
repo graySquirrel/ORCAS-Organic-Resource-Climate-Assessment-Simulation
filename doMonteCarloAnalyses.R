c24a1e61277e20f76eaf528c32f1d1d0b18ac8ed
graySquirrel <fritzebner@gmail.com>
2015-10-19 15:22:00 -0400

    plotFS  - shorter name...

--- doMonteCarloAnalyses.R
+++ doMonteCarloAnalyses.R
@@ -30,31 +30,32 @@ print(createPathwaysPlot(TRUE,s=AllStats))
 #####################################################################
 # sensitivity analysis...  start slow
 
-#par(mfrow=c(2,3)) # how many do you want to show?  rows x columns number of graphs
-plotFactorSensitivity(AllStats$ADstats,"Food waste","AD_Digester_CH4Leaks","CH4 Leaks","AD")
-plotFactorSensitivity(AllStats$ADstats,"Food waste","AD_MCFactor","utilization Factor","AD")
-plotFactorSensitivity(AllStats$ADstats,"Food waste","AD_Digester_parasiticLoad","parastic load","AD")
-plotFactorSensitivity(AllStats$ADstats,"Food waste","AD_reductionInVS","reduct in VS","AD")
-plotFactorSensitivity(AllStats$ADstats,"Food waste","AD_Storage_EFresidualMethaneM3CH4PerKgVS","storage resid methane","AD")
-plotFactorSensitivity(AllStats$ADstats,"Food waste","AD_xportTofield","xportToField","AD")
+par(mfrow=c(2,3)) # how many do you want to show?  rows x columns number of graphs
+plotFS(AllStats$ADstats,"Food waste","AD_Digester_CH4Leaks","CH4 Leaks","AD")
+plotFS(AllStats$ADstats,"Food waste","AD_MCFactor","utilization Factor","AD")
+plotFS(AllStats$ADstats,"Food waste","AD_Digester_parasiticLoad","parastic load","AD")
+plotFS(AllStats$ADstats,"Food waste","AD_reductionInVS","reduct in VS","AD")
+plotFS(AllStats$ADstats,"Food waste","AD_Storage_EFresidualMethaneM3CH4PerKgVS","storage resid methane","AD")
+plotFS(AllStats$ADstats,"Food waste","AD_xportTofield","xportToField","AD")
 
-#par(mfrow=c(2,3)) # how many do you want to show?  rows x columns number of graphs
-plotFactorSensitivity(AllStats$ADfstats,"Baked goods","AD_Digester_CH4Leaks","CH4 Leaks","ADf")
-plotFactorSensitivity(AllStats$ADfstats,"Baked goods","AD_MCFactor","utilization Factor","ADf")
-plotFactorSensitivity(AllStats$ADfstats,"Baked goods","AD_Digester_parasiticLoad","parastic load","ADf")
-plotFactorSensitivity(AllStats$ADfstats,"Baked goods","AD_reductionInVS","reduct in VS","ADf")
-plotFactorSensitivity(AllStats$ADfstats,"Baked goods","AD_Storage_EFresidualMethaneM3CH4PerKgVS","storage resid methane","ADf")
-plotFactorSensitivity(AllStats$ADfstats,"Baked goods","AD_xportTofield","xportToField","ADf")
 
-#par(mfrow=c(2,3)) # how many do you want to show?  rows x columns number of graphs
-plotFactorSensitivity(AllStats$CMstats,"Unsweetened dry goods","Compost_dieseLlpert","diesel","CM")
-plotFactorSensitivity(AllStats$CMstats,"Unsweetened dry goods","CompostPercentCdegraded","percent C degraded","CM")
-plotFactorSensitivity(AllStats$CMstats,"Unsweetened dry goods","Compost_degradedC_CH4","degraded C CH4","CM")
-plotFactorSensitivity(AllStats$CMstats,"Unsweetened dry goods","Compost_N2OperN","N20 per N","CM")
-plotFactorSensitivity(AllStats$CMstats,"Unsweetened dry goods","Compost_CS_factor","storage factor","CM")
-plotFactorSensitivity(AllStats$CMstats,"Unsweetened dry goods","Compost_N_loss","compost N remaining","CM")
+par(mfrow=c(2,3)) # how many do you want to show?  rows x columns number of graphs
+plotFS(AllStats$ADfstats,"Baked goods","AD_Digester_CH4Leaks","CH4 Leaks","ADf")
+plotFS(AllStats$ADfstats,"Baked goods","AD_MCFactor","utilization Factor","ADf")
+plotFS(AllStats$ADfstats,"Baked goods","AD_Digester_parasiticLoad","parastic load","ADf")
+plotFS(AllStats$ADfstats,"Baked goods","AD_reductionInVS","reduct in VS","ADf")
+plotFS(AllStats$ADfstats,"Baked goods","AD_Storage_EFresidualMethaneM3CH4PerKgVS","storage resid methane","ADf")
+plotFS(AllStats$ADfstats,"Baked goods","AD_xportTofield","xportToField","ADf")
 
-#par(mfrow=c(1,3)) # how many do you want to show?  rows x columns number of graphs
-plotFactorSensitivity(AllStats$LFstats,"Sweet dry goods","LFDieseluseLpert","diesel","LF")
-plotFactorSensitivity(AllStats$LFstats,"Sweet dry goods","Landfill_Oxidation_Factor","ox factor","LF")
-plotFactorSensitivity(AllStats$LFstats,"Sweet dry goods","LCEMax","LCEMax","LF")
+par(mfrow=c(2,3)) # how many do you want to show?  rows x columns number of graphs
+plotFS(AllStats$CMstats,"Unsweetened dry goods","Compost_dieseLlpert","diesel","CM")
+plotFS(AllStats$CMstats,"Unsweetened dry goods","CompostPercentCdegraded","percent C degraded","CM")
+plotFS(AllStats$CMstats,"Unsweetened dry goods","Compost_degradedC_CH4","degraded C CH4","CM")
+plotFS(AllStats$CMstats,"Unsweetened dry goods","Compost_N2OperN","N20 per N","CM")
+plotFS(AllStats$CMstats,"Unsweetened dry goods","Compost_CS_factor","storage factor","CM")
+plotFS(AllStats$CMstats,"Unsweetened dry goods","Compost_N_loss","compost N remaining","CM")
+
+par(mfrow=c(1,3)) # how many do you want to show?  rows x columns number of graphs
+plotFS(AllStats$LFstats,"Sweet dry goods","LFDieseluseLpert","diesel","LF")
+plotFS(AllStats$LFstats,"Sweet dry goods","Landfill_Oxidation_Factor","ox factor","LF")
+plotFS(AllStats$LFstats,"Sweet dry goods","LCEMax","LCEMax","LF")
