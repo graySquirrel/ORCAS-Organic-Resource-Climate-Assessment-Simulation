1eaadb73e58580e3187d5677dac26c750dde761a
jhe5003 <jhe5003@rit.edu>
2015-10-21 11:53:59 -0400

    update to read GF from file

--- unitTestLandfill.R
+++ unitTestLandfill.R
@@ -3,15 +3,15 @@
 
 source("treatmentClasses.R") 
 source("treatmentLandfill.R") 
-
+source("parseGlobalFactors.R")
 
 f1 <- Feedstock(type="OFMSW",TS=0.3,VS=0.90,Bo=334,TKN=5600,
                 percentCarboTS = 0.8, percentProteinTS = 0.10, percentLipidTS = 0.10,
-                fdeg = 0.85)
+                fdeg = 0.84)
 
 print(paste("TS",f1$TS,"Lo ",f1$Lo," TVS ",f1$TVS,"initialC ",f1$InitialC))
-g1 <- GlobalFactors()
+g1 <- getGlobalFactorsFromFile(doRanges = FALSE,verbose = TRUE)
 res <- LandfillTreatmentPathway(f1, g1, debug = T)
 print(res)
-res <- LandfillTreatmentPathway(f1, g1, debug = T, sequesterCarbon = F)
-print(res)
+#res <- LandfillTreatmentPathway(f1, g1, debug = T, sequesterCarbon = F)
+#print(res)
