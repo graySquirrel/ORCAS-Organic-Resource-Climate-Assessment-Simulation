88eb28db963ba20f89e6f51211655c618d79966c
graySquirrel <fritzebner@gmail.com>
2015-11-16 21:10:28 -0500

    updated, remove LA, convert ranges to numeric, only vary 'Range' cols

--- simpletonSensitivity2.R
+++ simpletonSensitivity2.R
@@ -14,7 +14,7 @@ f <- read.csv(file="Feedstock.csv",sep = ",",stringsAsFactors=FALSE)
 f1 <- Feedstock(type=f$Feedstock, TS=f$TS, VS=f$VS, Bo=f$Bo, TKN=f$TKN,
                 percentCarboTS=f$PercentCarboTS,percentProteinTS=f$PercentproteinTS,
                 percentLipidTS=f$PercentlipidTS,
-                fdeg=f$fdeg,TDN=f$TDN, Phosphorus=f$Phosphorus, Potassium=f$Potassium)
+                fdeg=f$fdeg,Phosphorus=f$Phosphorus, Potassium=f$Potassium)
 
 GFmemfile <- read.csv("Globalfactors.csv",stringsAsFactors = FALSE)
 glob <- getGlobalFactorsFromFile(doRanges = FALSE)
@@ -24,14 +24,15 @@ singleValueSensitivityByRange <- function(pathway,
                                           factorName,
                                           FUN,
                                           f1) {
+    print(paste("FACTORNAME",factorName))
     g1 <- getGlobalFactorsFromFile(doRanges = FALSE)
-    nom <- g1[[factorName]]
+    nom <- as.numeric(g1[[factorName]])
     outNom <- FUN(f1, g1, debug = F)[[1]]
-    low <- GFmemfile[GFmemfile$sw.name==factorName,"Range.Lo"][[1]]# elim multiple entries 
-    g1[[factorName]] <- low 
+    low <- GFmemfile[GFmemfile$sw.name==factorName,"Low"][[1]]# elim multiple entries 
+    g1[[factorName]] <- as.numeric(low) 
     outLo <- FUN(f1, g1, debug = F)[[1]]
-    high <- GFmemfile[GFmemfile$sw.name==factorName,"Range.High"][[1]]# elim multiple entries 
-    g1[[factorName]] <- high 
+    high <- GFmemfile[GFmemfile$sw.name==factorName,"High"][[1]]# elim multiple entries 
+    g1[[factorName]] <- as.numeric(high)
     outHi <- FUN(f1, g1, debug = F)[[1]]
     data.frame(f1$type, pathway, factorName, nom, outNom, low, outLo, high, outHi)
 }
@@ -42,10 +43,10 @@ fl$AD <- AnaerobicDigestionTreatmentPathway
 fl$CM <- compostTreatmentPathway
 fl$AF <- AnimalFeedTreatmentPathway
 fl$LF <- LandfillTreatmentPathway
-fl$LA <- LandApplicationTreatmentPathway
+#fl$LA <- LandApplicationTreatmentPathway
 
 for (i in 1:length(GFmemfile[,1])){
-    if (GFmemfile[i,1] != "" ) {
+    if (GFmemfile[i,1] != "" && GFmemfile[i,5] == "Range") {
         pw <- GFmemfile[i,1]
         gf <- GFmemfile[i,"sw.name"]
         val <- glob[[gf]]
@@ -59,7 +60,7 @@ for (i in 1:length(GFmemfile[,1])){
                 o <- rbind(o,singleValueSensitivityByRange("CM",gf,compostTreatmentPathway,f1))
                 o <- rbind(o,singleValueSensitivityByRange("AF",gf,AnimalFeedTreatmentPathway,f1))
                 o <- rbind(o,singleValueSensitivityByRange("LF",gf,LandfillTreatmentPathway,f1))
-                o <- rbind(o,singleValueSensitivityByRange("LA",gf,LandApplicationTreatmentPathway,f1))
+#                o <- rbind(o,singleValueSensitivityByRange("LA",gf,LandApplicationTreatmentPathway,f1))
             } else {
                 FUN <- fl[[GFmemfile[i,1]]]
                 ifelse(is.null(o),
