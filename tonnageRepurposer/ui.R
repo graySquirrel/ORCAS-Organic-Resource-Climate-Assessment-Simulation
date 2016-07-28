0bced13540e43d6e5b0c3e3ec24471ac47e856eb
graySquirrel <fritzebner@gmail.com>
2016-07-28 09:25:25 -0400

    updated some text

--- tonnageRepurposer/ui.R
+++ tonnageRepurposer/ui.R
@@ -1,32 +1,12 @@
 library(shiny)
 library(shinyTable)
-#' Define UI for application that demonstrates a simple Handsontable
-#' @author Jeff Allen \email{jeff@@trestletech.com}
-# shinyUI(
-#     pageWithSidebar(
-#         # Application title
-#         headerPanel("Move your waste!"),
-#         sidebarPanel(
-#             h3("Instructions")#,
-#             #             tableOutput("resTab")
-#         ),
-#         mainPanel(
-#             h3("Compare Scenarios.   Enter waste in MT."),
-#             htable("tblTo", colHeaders="provided"),
-#             h3("Results"),
-#             tableOutput("resTab"),
-#             plotOutput("resPlot")
-#         )     
-#     )
-# )
-
-
 
 shinyUI(fluidPage(
-    headerPanel("Move your waste!"),
+    headerPanel("ORCAS Tonnage repurposer"),
     fluidRow(
         column(2,
-               h3("Instructions")
+               h3("Instructions"),
+               h6("input tonnes of waste in old and new section and see how different the net emisisons are.")
         ),
         column(6,
                h3("Compare Scenarios.   Enter waste in MT."),
